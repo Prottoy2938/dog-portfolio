962940a245c1d0a8d6ddcba9b9888999b0d8e802
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-01-01 16:17:50 +0600

    Updates

---
+++ precache-manifest.800f630bd44ed55935995e65992152e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2940d95a760573761812e2b0f96ab200",
+    "url": "/dog-portfolio/index.html"
+  },
+  {
+    "revision": "aacd1a828f08fc8eb13c",
+    "url": "/dog-portfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "4c56669144a22774d3db",
+    "url": "/dog-portfolio/static/css/main.3cce36f1.chunk.css"
+  },
+  {
+    "revision": "aacd1a828f08fc8eb13c",
+    "url": "/dog-portfolio/static/js/2.706de0f5.chunk.js"
+  },
+  {
+    "revision": "9ff1df13142c1e6716bd2e1faf417c84",
+    "url": "/dog-portfolio/static/js/2.706de0f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c56669144a22774d3db",
+    "url": "/dog-portfolio/static/js/main.abe49512.chunk.js"
+  },
+  {
+    "revision": "10b2d7dfca1aebb874f2",
+    "url": "/dog-portfolio/static/js/runtime-main.bbda7315.js"
+  }
+]);
